dd05ee972d43becfe788024304cc1319295a906e
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-06-19 18:36:42 -0400

    Added cmd line arument to initialize with JSON

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,44 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/rgehrsitz/rulegopher/api/handler"
 	"github.com/rgehrsitz/rulegopher/api/middleware"
 	"github.com/rgehrsitz/rulegopher/pkg/engine"
 	"github.com/rgehrsitz/rulegopher/pkg/facts"
+	"github.com/rgehrsitz/rulegopher/pkg/rules"
 )
 
 func main() {
-	// Parse the command line arguments for the port
+	// Parse the command line arguments for the port and rules file
 	port := flag.String("port", "8080", "port to listen on")
+	logging := flag.Bool("logging", false, "enable or disable logging")
+	rulesFile := flag.String("rules", "", "JSON file containing the rules")
+
 	flag.Parse()
 
 	// Create the rules engine and fact handler
 	rulesEngine := engine.NewEngine()
 	factHandler := facts.NewFactHandler(rulesEngine)
 
+	// If a rules file is provided, load the rules from the file
+	if *rulesFile != "" {
+		file, err := os.Open(*rulesFile)
+		if err != nil {
+			fmt.Println("Failed to open rules file:", err)
+			return
+		}
+		defer file.Close()
+
+		var rules []rules.Rule
+		if err := json.NewDecoder(file).Decode(&rules); err != nil {
+			fmt.Println("Failed to decode rules file:", err)
+			return
+		}
+
+		for _, rule := range rules {
+			if err := rulesEngine.AddRule(rule); err != nil {
+				fmt.Println("Failed to add rule:", err)
+				return
+			}
+		}
+	}
+
 	// Create the API handler
 	apiHandler := handler.NewHandler(rulesEngine, factHandler)
 
 	// Set up the routes
-	// http.HandleFunc("/addRule", apiHandler.AddRule)
-	// http.HandleFunc("/removeRule", apiHandler.RemoveRule)
-	// http.HandleFunc("/evaluateFact", apiHandler.EvaluateFact)
-
-	// The following are equivalent to the above but with logging middleware
-	//	http.Handle("/addRule", middleware.LoggingMiddleware(http.HandlerFunc(apiHandler.AddRule)))
-	fmt.Println("Starting server...")
-	http.Handle("/addRule", middleware.LoggingMiddleware(http.HandlerFunc(apiHandler.AddRule)))
-	fmt.Println("Registered /addRule route")
-
-	http.Handle("/removeRule", middleware.LoggingMiddleware(http.HandlerFunc(apiHandler.RemoveRule)))
-	fmt.Println("Registered /removeRule route")
-
-	http.Handle("/evaluateFact", middleware.LoggingMiddleware(http.HandlerFunc(apiHandler.EvaluateFact)))
-	fmt.Println("Registered /evaluateFact route")
+	if *logging {
+		http.Handle("/addRule", middleware.LoggingMiddleware(http.HandlerFunc(apiHandler.AddRule)))
+		http.Handle("/removeRule", middleware.LoggingMiddleware(http.HandlerFunc(apiHandler.RemoveRule)))
+		http.Handle("/evaluateFact", middleware.LoggingMiddleware(http.HandlerFunc(apiHandler.EvaluateFact)))
+	} else {
+		http.Handle("/addRule", http.HandlerFunc(apiHandler.AddRule))
+		http.Handle("/removeRule", http.HandlerFunc(apiHandler.RemoveRule))
+		http.Handle("/evaluateFact", http.HandlerFunc(apiHandler.EvaluateFact))
+	}
 
 	// Start the server
 	fmt.Printf("Starting server on port %s\n", *port)
