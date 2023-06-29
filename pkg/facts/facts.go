c33a4694c40a23f7b0aebad2ea21cc4fa1e3f301
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-06-28 20:36:55 -0400

    Updated comments and variable names in facts.go

--- pkg/facts/facts.go
+++ pkg/facts/facts.go
@@ -5,16 +5,19 @@ import (
 	"github.com/rgehrsitz/rulegopher/pkg/rules"
 )
 
+// FactHandler is used to handle facts and evaluate them using an engine.
 type FactHandler struct {
 	engine *engine.Engine
 }
 
+// NewFactHandler creates a new FactHandler with the given engine.
 func NewFactHandler(engine *engine.Engine) *FactHandler {
 	return &FactHandler{
 		engine: engine,
 	}
 }
 
-func (fh *FactHandler) HandleFact(fact rules.Fact) []rules.Event {
-	return fh.engine.Evaluate(fact)
+// HandleFact evaluates the given fact using the FactHandler's engine.
+func (factHandler *FactHandler) HandleFact(fact rules.Fact) []rules.Event {
+	return factHandler.engine.Evaluate(fact)
 }
