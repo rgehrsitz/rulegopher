4c34c330f2de4869752ac40bbf78ab081baf8e4f
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-07-22 12:19:08 -0400

    still debugging

--- pkg/rules/rules.go
+++ pkg/rules/rules.go
@@ -110,11 +110,17 @@ func (r *Rule) Validate() error {
 		"notContains":        true,
 	}
 
-	// Function to validate a single condition
-	validateCondition := func(condition Condition) error {
-		if _, ok := validOperators[condition.Operator]; !ok {
-			return fmt.Errorf("invalid operator: %s", condition.Operator)
+	// Declare the function variable
+	var validateCondition func(condition Condition) error
+
+	// Assign the function body to the variable
+	validateCondition = func(condition Condition) error {
+		if condition.Fact != "" && condition.Operator != "" {
+			if _, ok := validOperators[condition.Operator]; !ok {
+				return fmt.Errorf("invalid operator: %s", condition.Operator)
+			}
 		}
+
 		// Validate nested conditions
 		for _, nestedCondition := range condition.All {
 			if err := validateCondition(nestedCondition); err != nil {
