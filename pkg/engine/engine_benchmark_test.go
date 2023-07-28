ee3f0c6a4edb77504452aa5d7e500351b586c663
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-07-28 12:36:16 -0400

    Moved new benchmark test to engine folder

--- pkg/engine/engine_benchmark_test.go
+++ pkg/engine/engine_benchmark_test.go
@@ -1,8 +1,12 @@
 package engine_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/rgehrsitz/rulegopher/pkg/engine"
 	"github.com/rgehrsitz/rulegopher/pkg/rules"
@@ -141,4 +145,72 @@ func BenchmarkEngine_EvaluateRule_Performance(b *testing.B) {
 			b.Fatalf("Failed to evaluate facts: %v", err)
 		}
 	}
+}
+
+func BenchmarkLargeRuleset(b *testing.B) {
+
+	numIterations := 100
+
+	// Read rules.json into a string
+	rulesJSON := readFile("rules.json")
+
+	// Unmarshal into a Rules struct
+	var ruleSet []rules.Rule
+	json.Unmarshal([]byte(rulesJSON), &ruleSet)
+
+	engine := engine.NewEngine()
+
+	for _, rule := range ruleSet {
+		engine.AddRule(rule)
+	}
+
+	// Read facts.json into a string
+	factsJSON := readFile("facts.json")
+
+	// Unmarshal into a []Fact
+	var facts []rules.Fact
+	json.Unmarshal([]byte(factsJSON), &facts)
+
+	// Benchmark Evaluate
+	start := time.Now()
+
+	for i := 0; i < numIterations; i++ {
+		for _, fact := range facts {
+			engine.Evaluate(fact)
+		}
+	}
+
+	// Benchmarking stats
+	totalDuration := time.Since(start)
+
+	fmt.Println("Total duration:", totalDuration)
+
+	numRules := len(ruleSet)
+	numFacts := len(facts)
+
+	perIteration := totalDuration / time.Duration(numIterations)
+
+	fmt.Println("Duration per iteration:", perIteration)
+
+	// Print stats
+	fmt.Println("Num rules:", numRules)
+	fmt.Println("Num facts:", numFacts)
+	fmt.Println("Num iterations:", numIterations)
+
+	// Calculate facts/sec
+	factsPerSec := float64(numFacts*numIterations) / totalDuration.Seconds()
+	fmt.Println("Facts per second:", factsPerSec)
+
+	// Calculate rules/sec
+	rulesPerSec := float64(numRules*numIterations) / totalDuration.Seconds()
+	fmt.Println("Rules per second:", rulesPerSec)
+
+}
+
+func readFile(filename string) string {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(content)
 }
